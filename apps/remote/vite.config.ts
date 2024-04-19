b6e56327d56f3364d1ffd277d09b6ed2d489668e
byj0101 <byj010101@gmail.com>
2024-04-19 15:06:13 +0900

    절대경로 설정

--- apps/remote/vite.config.ts
+++ apps/remote/vite.config.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+      components: `${path.resolve(__dirname, "./src/components")}`,
+      public: `${path.resolve(__dirname, "./public")}`,
+      pages: path.resolve(__dirname, "./src/pages"),
+      types: `${path.resolve(__dirname, "./src/@types")}`,
+      containers: `${path.resolve(__dirname, "./src/containers")}`,
+      helpers: `${path.resolve(__dirname, "./src/helpers")}`,
+      modules: `${path.resolve(__dirname, "./src/modules")}`,
+      config: `${path.resolve(__dirname, "./src/config")}`,
+      material: `${path.resolve(__dirname, "./src/material")}`,
+      lib: `${path.resolve(__dirname, "./src/lib")}`,
+      images: `${path.resolve(__dirname, "./src/images")}`,
+      hooks: `${path.resolve(__dirname, "./src/hooks")}`,
+      locales: `${path.resolve(__dirname, "./src/locales")}`,
+    },
+  },
   plugins: [
     react(),
     federation({
